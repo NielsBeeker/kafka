de5e1bcd1c5fdc908363360e14c0311557ca1ff0
Louis Arrignon <louis.arrignon@epita.fr>
2022-01-19 15:40:29 +0100

    Add services functions

--- src/main/java/kafka/services/EventBusService.java
+++ src/main/java/kafka/services/EventBusService.java
@@ -1,5 +1,46 @@
 package kafka.services;
 
+import kafka.models.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class EventBusService {
+    EventBus eventBus;
+
+    public boolean subscribe(Topic topic, ConsumerGroup consumerGroup) {
+        if (consumerGroup.getTopicId())
+            return false;
+        consumerGroup.setTopicId(topic.topicId);
+        return true;
+    }
+
+    public void publish(Topic topic, Event event) {
+        try{
+           this.eventBus.getTopics().get(topic.subject)
+           .getPartition(event.getPartitionId().addEvent(event);
+        }
+        catch (Error er) {
+            System.err.println(er);
+        }
+    }
+
+    public ArrayList<Event> poll(String subject, Consumer consumer, Integer nbEvent, Integer timeout) {
+        Topic topic = this.eventBus.getTopics().get(subject);
+        ArrayList<Event> events = new ArrayList<>();
+        for(var partition : topic.getPartitions()){
+            if (consumer.partitionsId.contains(partition.partionId)) {
+                if (events.size() == nbEvent)
+                    break;
+                for (var event : partition) {
+                    if (events.size() == nbEvent)
+                        break;
+                    events.add(event);
+                }
+            }
+        }
+        return events;
+    }
+
 
 }
