7afa56f2bab55b86f49c33100082d3aeb36275f7
Louis SANTUCCI <louis.santucci@epita.fr>
2022-01-22 17:45:16 +0100

    [CONSUMER] removed useless id for consumer

--- src/main/java/kafka/models/Consumer.java
+++ src/main/java/kafka/models/Consumer.java
@@ -3,13 +3,10 @@ package kafka.models;
 import java.util.ArrayList;
 
 public class Consumer {
-    // ID
-    private final Integer consumerId;
     private ArrayList<Integer> partitionsId;
     // Consumer group ID
 
-    public Consumer(Integer consumerId) {
-        this.consumerId = consumerId;
+    public Consumer() {
         this.partitionsId = new ArrayList<>();
     }
 
